b8fd4df122b45ef5b1f20e6276f694268f6adeb1
拓仲(牟云龙) <muylzh@163.com>
2020-04-07 11:57:09 +0800

    Initial commit

---
+++ src/main/java/com/github/StandardParentApplication.java
@@ -0,0 +1,13 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StandardParentApplication.class, args);
+    }
+
+}
